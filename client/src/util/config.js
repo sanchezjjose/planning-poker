ade9170db1b08ecd9be6de01481b4d4dd5ef75ae
Jose Sanchez <sanchezjjose@gmail.com>
2021-10-07 22:08:47 -0400

    styling updates

--- client/src/util/config.js
+++ client/src/util/config.js
@@ -2,4 +2,4 @@ export const endpoint = process.env.NODE_ENV === 'production'
   ? 'https://planning-poker-nodejs.ue.r.appspot.com'
   : 'http://localhost:3001'
 
-export const points = ['0', '0.5', '1', '2', '3', '5', '8', '13', '?'];
+export const points = ['0', '0.5', '1', '2', '3', '5', '8', '13'];
